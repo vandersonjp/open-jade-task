f027ede6ab4cd18ace7ddfa06996e2fed616b58e
botelhojp <botelhojp@gmail.com>
2013-03-11 22:40:58 -0300

    ajusta do main

--- src/main/java/openjade/task/run/Boot.java
+++ src/main/java/openjade/task/run/Boot.java
@@ -7,10 +7,12 @@ public class Boot {
 	protected static Logger log = Logger.getLogger(Boot.class);
 
 	public static void main(String[] args) {
-		
-		openjade.Boot.loadXml();
-		
-		
+		if (args != null && args.length > 0) {
+			openjade.Boot.main(args);
+		} else {
+			openjade.Boot.loadXml();
+		}
+//		openjade.Boot.loadXml();
 //		String[] container = { "-gui", "-host", "127.0.0.1", "-monitor" };
 //		
 //		openjade.Boot.main(container);
@@ -23,23 +25,23 @@ public class Boot {
 //		loadAgents( 18,20, "agent", "openjade.task.agent.TaskAgent", "Agents-Container", "123456", "openjade.trust.NothingModel", "EXCELLENT");
 	}
 
-	private static void loadAgents(int begin, int end, String _nickName, String className, String container, String pass, String _trustModel, String _ability) {
-		for (int i = begin; i < end + 1; i++) {
-			String nickName = _nickName + "_" + format("000", i);
-			String cert = "/certs/" + nickName + ".pfx";
-			
-			String trustModel = (_trustModel == null ) ? "" : ", " + _trustModel;
-			String ability = (_ability == null ) ? "" : ", " + _ability;
-			
-			
-			String[] cont = { "-host", "127.0.0.1", "-container", "-container-name", container, nickName + ":" + className + "(" + cert + ", " + pass + ", " + container + trustModel + ability + ")" };
-			openjade.Boot.main(cont);
-		}
-	}
-
-	private static String format(String _number, int i) {
-		String number = _number + i;
-		int dif = number.length() - _number.length();
-		return number.substring(dif);
-	}
+//	private static void loadAgents(int begin, int end, String _nickName, String className, String container, String pass, String _trustModel, String _ability) {
+//		for (int i = begin; i < end + 1; i++) {
+//			String nickName = _nickName + "_" + format("000", i);
+//			String cert = "/certs/" + nickName + ".pfx";
+//			
+//			String trustModel = (_trustModel == null ) ? "" : ", " + _trustModel;
+//			String ability = (_ability == null ) ? "" : ", " + _ability;
+//			
+//			
+//			String[] cont = { "-host", "127.0.0.1", "-container", "-container-name", container, nickName + ":" + className + "(" + cert + ", " + pass + ", " + container + trustModel + ability + ")" };
+//			openjade.Boot.main(cont);
+//		}
+//	}
+//
+//	private static String format(String _number, int i) {
+//		String number = _number + i;
+//		int dif = number.length() - _number.length();
+//		return number.substring(dif);
+//	}
 }
